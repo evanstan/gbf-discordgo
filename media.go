5b2d736c08f06ab37efb70345c9d0dbfcd9a2d33
Evans <treva_ivan@hotmail.com>
2018-04-05 01:02:25 +0900

    change media url

--- media.go
+++ media.go
@@ -1,7 +1,7 @@
 package gbfbot
 
 const (
-	GBFLogoImageURL    = "https://upload.wikimedia.org/wikipedia/en/e/e5/Granblue_Fantasy_logo.png"
+	GBFLogoImageURL    = "./media/Granblue_Fantasy_logo.png"
 	GBFLogoImageWidth  = 273
 	GBFLogoImageHeight = 187
 )
